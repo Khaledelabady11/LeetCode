9483a0e2bd796f898f99d015df872dcd68a46175
khaledelabady <65457778+Khaledelabady11@users.noreply.github.com>
2022-11-02 14:34:08 +0200

    Time: 170 ms (13.33%), Space: 211.4 MB (60.00%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.rb
@@ -0,0 +1,10 @@
+# @param {Integer[]} nums
+# @param {Integer} target
+# @return {Boolean}
+def search(nums, target)
+    if nums.include?(target)
+        return true 
+    else
+        return false
+    end
+end
